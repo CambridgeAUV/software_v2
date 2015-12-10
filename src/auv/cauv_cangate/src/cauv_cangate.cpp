b59a99d10639c6e04d1a3a1e5fce046464370fbd
Edward Liu <yl573@data.cambridgeauv.co.uk>
2015-12-10 14:46:58 +0000

    Changed cauv_cangate to output depth.

--- src/auv/cauv_cangate/src/cauv_cangate.cpp
+++ src/auv/cauv_cangate/src/cauv_cangate.cpp
@@ -25,6 +25,8 @@ to the motor control board.
 #include <cauv_cangate/msg_motor_demand.h>
 #include <cauv_cangate/msg_pressure_status.h>
 
+#include <std_msgs/Float64.h>
+
 int mysocket;
 
 void cauv_motor_send_command(const cauv_cangate::msg_motor_demand::ConstPtr motor_control_message)
@@ -43,6 +45,54 @@ void cauv_motor_send_command(const cauv_cangate::msg_motor_demand::ConstPtr moto
 	write(mysocket, &frame, sizeof(struct can_frame)); 
 }
 
+//added the class pressure_depth_converter
+//needs to include cauv_control::DepthCalibration
+class pressure_depth_converter
+{
+   private:
+	float forward_pressure;
+	float back_pressure;
+        float fore_offset;
+        float aft_offset;
+        float fore_multiplier;
+        float aft_multiplier;
+   public:
+	int16_t fwd_pressure;
+	int16_t aft_pressure;
+
+
+	void retrieve_calibrations(ros::NodeHandle &mynode)
+	{
+		//Default values not yet set
+		mynode.param("cauv_cangate/fore_offset",fore_offset,float(1.0));
+		mynode.param("cauv_cangate/aft_offset",aft_offset,float(1.0));
+		mynode.param("cauv_cangate/fore_multiplier",fore_multiplier,float(1.0));
+		mynode.param("cauv_cangate/aft_multiplier",aft_multiplier,float(1.0));
+	}
+
+	float calc_depth()
+	{
+		forward_pressure = fwd_pressure;
+		back_pressure = aft_pressure;
+		return (depthFromForePressure(forward_pressure)+depthFromAftPressure(back_pressure))/2;
+	}
+
+	float depthFromForePressure(float const& pressure) const
+        {
+ 		return fore_offset + fore_multiplier * pressure;
+
+        }
+
+        float depthFromAftPressure(float const& pressure) const
+        {
+		return aft_offset + aft_multiplier * pressure;
+        }	
+};
+
+
+        
+
+
 
 int main(int argc, char **argv)
 {
@@ -52,15 +102,27 @@ int main(int argc, char **argv)
 	struct ifreq ifr;
 	struct can_frame last_frame_in_buffer;
 
+
 	//ROS initialisation
 	ros::init(argc, argv, "cauv_cangate");
 	ros::NodeHandle mynode;
 	ros::Rate loopRate(100);
 	
 	ros::Subscriber sub_motor_command = mynode.subscribe("cauv_motor_demand", 1, cauv_motor_send_command);
-	ros::Publisher pub_pressure_status = mynode.advertise<cauv_cangate::msg_pressure_status>("cauv_pressure_status", 1);
+
 	
-	cauv_cangate::msg_pressure_status pressure_status_message;
+
+
+//publishes the depth of the midpoint of the auv
+//changed from here
+	pressure_depth_converter depth_track = pressure_depth_converter();
+
+	depth_track.retrieve_calibrations(mynode);
+
+	ros::Publisher pub_depth_status = mynode.advertise<std_msgs::Float64>("cauv_cangate/cauv_depth_status", 1);	
+	std_msgs::Float64 depth;
+//to here
+
 
 	ROS_INFO("Initialising the cauv_cangate node");
 	
@@ -99,14 +161,18 @@ int main(int argc, char **argv)
 		{
 			if(last_frame_in_buffer.data[4] == 0)
 			{
-				memcpy(&pressure_status_message.fwd_pressure, &last_frame_in_buffer.data, 2);
+				memcpy(&depth_track.fwd_pressure, &last_frame_in_buffer.data, 2);
+
 			}
 			
 			if(last_frame_in_buffer.data[4] == 1)
 			{
-				memcpy(&pressure_status_message.aft_pressure, &last_frame_in_buffer.data, 2);
+				memcpy(&depth_track.aft_pressure, &last_frame_in_buffer.data, 2);
 			}
-			pub_pressure_status.publish(pressure_status_message);
+			
+			depth.data = depth_track.calc_depth();
+
+			pub_depth_status.publish(depth);
 		}
 		
 		//Activate callback!
