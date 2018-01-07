560565b082c76f38dc61e87bcee33a43f30da75f
ZhangShaojie1127 <33404725+ZhangShaojie1127@users.noreply.github.com>
2018-01-07 20:46:17 +0800

    Add files via upload

---
+++ old jason.py
@@ -0,0 +1,31 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Read input
+import sys
+Inputs=[]
+def add(Input):
+    List.append(Input)
+    
+for line in sys.stdin:
+    
+    Inputs.append(line[:-1])
+List=[]
+
+
+
+    
+def get(Input):
+    properties=[]
+    quotation=0
+    Bigbracket=0
+    Listbracket=0
+    Boolean=False
+    for j in range(0,len(Input)):
+        if not Boolean:
+            String=""
+            
+    
+    
+    
+def delete(Input):
+    
+    '''
